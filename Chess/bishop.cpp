e853163271de58a9d080ceab8f8eb075d3b22d51
Andreibatman <oficialandreicroitoriu@yahoo.com>
2022-11-16 18:47:43 +0200

    Cleaned the code for the bishop

--- Chess/bishop.cpp
+++ Chess/bishop.cpp
@@ -1,36 +1,36 @@
 #include "bishop.h"
 
-Bishop::Bishop(int row,int column){
+Bishop::Bishop(int row, int column){
     this->row=row;
     this->column=column;
 }
 
 /**
- * Functionality to move a bishop by diffRow rows and diffRow columns
- * @params
- * diffRow -> difference in rows (positive - up, negative - down)
- * diffColumn -> difference in columns (positive - right, negative - left)
+ @inheritdoc
 */
-void Bishop::makeMove(int diffRow,int diffColumn){
+void Bishop::makeMove(int diffRow, int diffColumn){
     if(!isValidMove(diffRow,diffColumn))
         return;
+    
     setRow(this->row + diffRow);
     setColumn(this->column + diffColumn);
 }
 
-bool Bishop::isValidMove(int diffRow,int diffColumn){
-    
-    bool ok = true, diagonal = false;
-    int newRow=this->row+diffRow;
-    int newColumn=this->column+diffColumn;
 
-    ok = ok && (newRow>=1 && newRow<=8);
-    ok = ok && (newColumn>=1 && newColumn<=8);
-    
-    diagonal = diagonal || (newRow+newColumn == this->row+this->column);   ///right to left diagonal
-    diagonal = diagonal || (newRow-newColumn == this->row-this->column);   ///left to right diagonal
+bool Bishop::isValidMove(int diffRow, int diffColumn){
+    return Piece::isValidMove(diffRow, diffColumn) && isMoveLegal(diffRow, diffColumn);
+}
 
-    ok = ok & diagonal;
+
+bool Bishop::isMoveLegal(int diffRow, int diffColumn)
+{
+    bool ok = false;
+
+    int newRow = this->row + diffRow;
+    int newColumn = this->column + diffColumn;
+
+    ok = ok || (newRow+newColumn == this->row+this->column);   ///right to left diagonal
+    ok = ok || (newRow-newColumn == this->row-this->column);   ///left to right diagonal
 
     return ok;
 }
--- Chess/bishop.h
+++ Chess/bishop.h
@@ -1,17 +1,19 @@
-#include "piece.h"
-
 #ifndef H_BISHOP
 #define H_BISHOP
 
+#include "piece.h"
 class Bishop: public Piece{
     public:
-        Bishop(int row,int column);
-        virtual void makeMove(int diffRow,int diffColumn);
+        Bishop(int row, int column);
+        virtual void makeMove(int diffRow, int diffColumn);
+
     protected:
-        virtual bool isValidMove(int diffRow,int diffColumn);
+        virtual bool isValidMove(int diffRow, int diffColumn);
+
     private:
         int row;
         int column;
+        bool isMoveLegal(int diffRow, int diffColumn);
 }
 
 
