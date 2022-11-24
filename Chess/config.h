3efb57226efefd850498c4af96e53d7eadfd8966
Rusu <rusudaniel5799@gmail.com>
2022-11-24 22:47:41 +0200

    Added painter structure

---
+++ Chess/config.h
@@ -0,0 +1,12 @@
+#ifndef H_CONFIG
+#define H_CONFIG
+
+const int WINDOW_HEIGHT = 760;
+const int WINDOW_WIDTH = 760;
+
+const int OFFSET_X = 100;
+const int OFFSET_Y = 100;
+
+const int SQUARE_SIZE = 70;
+
+#endif // H_CONFIG
