4452a3d1c47059ff4e7c79a7a5552d433e1caea2
Ciornache <107924694+Ciornache@users.noreply.github.com>
2022-11-30 21:02:21 +0200

    Updated config.h and painter.cpp

---
+++ config.h
@@ -0,0 +1,25 @@
+#ifndef H_CONFIG
+#define H_CONFIG
+
+const int WINDOW_HEIGHT = 760;
+const int WINDOW_WIDTH = 760;
+
+const int OFFSET_X = 100;
+const int OFFSET_Y = 100;
+
+const int SQUARE_SIZE = 70;
+
+
+char BlackOnBlackPaths[][105] = {"BlackOnBlack/knight.jpg",
+                                "BlackOnBlack/queen.jpg",
+                                "BlackOnBlack/bishop.jpg",
+                                "BlackOnBlack/rook.jpg",
+                                "BlackOnBlack/pawn.jpg"};
+
+char BlackOnWhitePaths[][105] = {"BlackOnWhite/knight.jpg",
+                                "BlackOnWhite/queen.jpg",
+                                "BlackOnWhite/bishop.jpg",
+                                "BlackOnWhite/rook.jpg",
+                                "BlackOnWhite/pawn.jpg"};
+
+#endif // H_CONFIG
